6590121e1a8c35e8a95d2bab616fbbdadc661c3a
Shivamyv <theshivam23679@gmail.com>
2026-05-05 12:20:04 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (63.05%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -11,31 +11,22 @@
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        
- if (head == nullptr || head->next == nullptr || k == 0) 
-            return head;
-
-        // Calculating length
-        ListNode* temp = head;
-        int length = 1;
-        while (temp->next != nullptr) {
-            ++length;
-            temp = temp->next;
+        if(head==nullptr || head->next==nullptr || k==0) return head; 
+        ListNode*temp=head;
+        int length=1;
+        while(temp->next!=nullptr){
+            length++;
+            temp=temp->next;
         }
-
-        // Link last node to first node
-        temp->next = head;
-        // When k is more than length of list
-        k = k % length; 
-        // To get end of the list
-        int end = length - k; 
-        while (end-- > 0) 
-            temp = temp->next;
-
-        // Breaking last node link and pointing to NULL
-        head = temp->next;
-        temp->next = nullptr;
-
+         temp->next=head;
+         k=k%length;
+         int end=length-k;
+         while(end-- >0){
+            temp=temp->next;
+         }
+         head=temp->next;
+        temp->next=nullptr;
         return head;
+
     }
 };
