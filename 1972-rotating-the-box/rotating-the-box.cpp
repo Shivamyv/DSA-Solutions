f8336f34f5d9566c892d2b1f6bfe795b1e004be8
Shivamyv <theshivam23679@gmail.com>
2026-05-06 13:16:34 +0530

    Time: 11 ms (65.85%) | Memory: 56.8 MB (38.33%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.cpp
+++ 1972-rotating-the-box/rotating-the-box.cpp
@@ -3,12 +3,11 @@ public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid) {
         int n=boxGrid.size();
         int m=boxGrid[0].size();
-
+   
         vector<vector<char>>ans(m,vector<char>(n));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 ans[j][i]=boxGrid[i][j];
-                
             }
         }
         for(int i=0;i<m;i++){
