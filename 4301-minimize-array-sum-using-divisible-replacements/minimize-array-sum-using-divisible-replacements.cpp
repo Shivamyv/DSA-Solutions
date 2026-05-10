6a56aa7b07704608e360939f3184adcbfaa169e3
Shivamyv <theshivam23679@gmail.com>
2026-05-10 18:09:30 +0530

    Time: 850 ms (29.41%) | Memory: 311.4 MB (6.72%) - LeetSync

---
+++ 4301-minimize-array-sum-using-divisible-replacements/minimize-array-sum-using-divisible-replacements.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    long long minArraySum(vector<int>& nums) {
+    int n=nums.size();
+   unordered_map<int,int>mpp;
+   for(auto it:nums){
+     mpp[it]++;
+   }
+   for(auto &x:nums){
+    vector<int>factorial;
+   for(int i=1;i*i<=x;i++){
+    if(x%i!=0) continue;
+    if(mpp[i]) {
+        x=i; 
+        break;
+    }
+    factorial.push_back(x/i);
+
+   }
+   while(!factorial.empty()){
+    int y=factorial.back();
+    factorial.pop_back();
+    if(mpp[y]){
+        x=min(x,y);
+         break;
+    }
+   }
+   }
+   return accumulate(nums.begin(),nums.end(),0LL);
+
+
+    }
+};
