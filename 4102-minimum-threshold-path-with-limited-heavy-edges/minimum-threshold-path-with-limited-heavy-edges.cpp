e085132ec6579eb3d7e436c8f77a3e3ee9be0e3d
Shivamyv <theshivam23679@gmail.com>
2026-05-14 13:57:04 +0530

    Time: 2056 ms (5.00%) | Memory: 542.2 MB (5.06%) - LeetSync

---
+++ 4102-minimum-threshold-path-with-limited-heavy-edges/minimum-threshold-path-with-limited-heavy-edges.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+ int bfs(int n, vector<vector<int>>& edges, int source, int target, int k,int threshold){
+        vector<pair<int,int>>adj[n];
+        for(auto it:edges){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+         vector<int> dist(n, INT_MAX);
+
+        deque<int> dq;
+
+        dist[source] = 0;
+        dq.push_front(source);
+
+        while(!dq.empty()) {
+
+            int node = dq.front();
+            dq.pop_front();
+
+            for(auto &it : adj[node]) {
+
+                int adjNode = it.first;
+                int wt = it.second;
+
+                // heavy edge => cost 1
+                int cost = (wt > threshold) ? 1 : 0;
+
+                if(dist[node] + cost < dist[adjNode]) {
+
+                    dist[adjNode] = dist[node] + cost;
+
+                    // 0-1 BFS
+                    if(cost == 0)
+                        dq.push_front(adjNode);
+                    else
+                        dq.push_back(adjNode);
+                }
+            }
+        }
+
+        return dist[target] <= k;
+
+
+
+
+ }
+
+
+    int minimumThreshold(int n, vector<vector<int>>& edges, int source, int target, int k) {
+        
+
+    if(source==target) return 0;
+
+
+        int low=0,high=1e9;
+        int ans=-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(bfs(n,edges,source,target,k,mid)){
+              ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+
+    return ans;
+
+
+    }
+};
