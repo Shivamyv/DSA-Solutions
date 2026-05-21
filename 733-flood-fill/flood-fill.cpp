f872d59f8ad8d6e4253ddd016bc9250945b5a11c
Shivamyv <theshivam23679@gmail.com>
2026-05-21 18:10:00 +0530

    Time: 8 ms (2.09%) | Memory: 18.5 MB (19.91%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,34 +1,45 @@
 class Solution {
-    private:
-    void dfs(int row,int col,vector<vector<int>>&ans, vector<vector<int>>&image,int color,int delrow[],
-    int delcol[],int inicolor){
-
-        ans[row][col]=color;
-        int n=image.size();
-        int m=image[0].size();
+public:
+  vector<vector<int>> bfs(int sr, int sc, int color, vector<vector<int>>&vis,vector<vector<int>>& image){
+     int m=image.size();
+       int n=image[0].size();
+    int incolor=image[sr][sc];
+    queue<pair<int,int>>q;
+    q.push({sr,sc});
+    vis[sr][sc]=true;
+     image[sr][sc]=color;
+    int dr[]={-1,0,+1,0};
+    int dc[]={0,+1,0,-1};
+   
+    while(!q.empty()){
+        auto it=q.front();
+        int nr=it.first;
+        int nc=it.second;
+        
+        q.pop();
         for(int i=0;i<4;i++){
-            int nrow=row+delrow[i];
-            int ncol=col+delcol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
-            image[nrow][ncol]==inicolor && ans[nrow][ncol] !=color){
-                dfs(nrow,ncol,ans,image,color,delrow,delcol,
-                inicolor);
-            }
+            int nrow=nr+dr[i];
+            int ncol=nc+dc[i];
+        
+        if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol] &&
+           image[nrow][ncol]==incolor){
+            image[nrow][ncol]=color;
+            q.push({nrow,ncol});
+            vis[nrow][ncol]=true;
+       
+       
         }
     }
+    }
+    return image;
 
-public:
+ }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, 
-    int sr, int sc, int color) {
-      int delrow[] = {-1, 0, 1, 0};
-      int delcol[] = {0, 1, 0, -1};
-      
-      int inicolor=image[sr][sc];
-      vector<vector<int>>ans=image;
-       dfs(sr,sc,ans,image,color,delrow,delcol,inicolor);
-       return ans;
-      
 
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+       int m=image.size();
+       int n=image[0].size();
+       vector<vector<int>>vis(m,vector<int>(n,false));
+       return bfs(sr,sc,color,vis,image);
     }
 };
