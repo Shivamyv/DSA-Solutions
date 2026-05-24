13a9a539ee0a0dcfa3d9547bdf02259956fce264
Shivam Kumar <theshivam23679@gmail.com>
2026-05-24 15:52:38 +0530

    Time: 7 ms (94.19%) | Memory: 19 MB (76.45%) - LeetSync

---
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -0,0 +1,79 @@
+// class Solution {
+// public:
+//   int f(int i,vector<int>& arr, int d, vector<int>dp){
+//     int n =arr.size();
+//      int result=1;
+//     if(dp[i]!=-1) return dp[i];
+
+//     for(int j=i-1;j>=max(0,i-d);j--){
+//         if(arr[j]>=arr[i]) break;
+//         result=max(result,1+f(j,arr,d,dp));
+//     }
+//     for(int j=i+1;j<=min(n-1,i+d);j++){
+//         if(arr[j]>=arr[i]) break;
+//         result=max(result,1+f(j,arr,d,dp));
+//     }
+
+//    return dp[i]=result;
+    
+      
+
+//   }
+
+//     int maxJumps(vector<int>& arr, int d) {
+//         int n=arr.size();
+//         int result=1;
+//          vector<int>dp(n+1,-1);
+//          for(int i=0;i<n;i++){
+//            result=max(result,f(i,arr,d,dp));
+//          }
+//       return result;
+
+//     }
+// };
+
+class Solution {
+public:
+
+    int f(int i, vector<int>& arr, int d, vector<int>& dp) {
+
+        if(dp[i] != -1) return dp[i];
+
+        int n = arr.size();
+        int result = 1;
+
+        // left
+        for(int j = i-1; j >= max(0, i-d); j--) {
+
+            if(arr[j] >= arr[i]) break;
+
+            result = max(result, 1 + f(j, arr, d, dp));
+        }
+
+        // right
+        for(int j = i+1; j <= min(n-1, i+d); j++) {
+
+            if(arr[j] >= arr[i]) break;
+
+            result = max(result, 1 + f(j, arr, d, dp));
+        }
+
+        return dp[i] = result;
+    }
+
+    int maxJumps(vector<int>& arr, int d) {
+
+        int n = arr.size();
+
+        vector<int> dp(n, -1);
+
+        int result = 1;
+
+        for(int i = 0; i < n; i++) {
+
+            result = max(result, f(i, arr, d, dp));
+        }
+
+        return result;
+    }
+};
