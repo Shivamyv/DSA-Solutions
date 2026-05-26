5f4db7014a8be2a952b2c93affbc84004be9c7a4
Shivam Kumar <theshivam23679@gmail.com>
2026-05-26 12:36:31 +0530

    Time: 47 ms (84.99%) | Memory: 21 MB (70.00%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,37 +1,35 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-        vector<string>& wordList){
-        int n=wordList.size();
-        queue<pair<string,int>>q;
-        q.push({beginWord,1});
-        unordered_set<string>st(wordList.begin(),wordList.end());
-        while(!q.empty()){
-            string word=q.front().first;
-            int steps=q.front().second;
-            q.pop();
+int bfs(string beginWord, string endWord, vector<string>& wordList){
+    queue<pair<string,int>>q;
+    unordered_set<string>st(wordList.begin(),wordList.end());
+    if(st.find(endWord)==st.end()) return 0;
+    q.push({beginWord,1});
+    while(!q.empty()){
+        string word=q.front().first;
+        int steps=q.front().second;
+        q.pop();
+        if(word==endWord) return steps;
 
-            if(word==endWord) return steps;
-            for(int i=0;i<word.size();i++){
-                char original=word[i];
-            for( char ch='a';ch<='z';ch++){
+        for(int i=0;i<word.size();i++){
+            char original=word[i];
+            for(char ch='a';ch<='z';ch++){
                 word[i]=ch;
-             if(st.find(word)!=st.end()){
-                st.erase(word);
-                q.push({word,steps+1});
-             }
+                if(st.find(word)!=st.end()){
+                    st.erase(word);
+                    q.push({word,steps+1});
 
+
+                }
             }
-
             word[i]=original;
-
-            
-            }
-
-
         }
-     
-     return 0;
-        
+    }
+
+    return 0;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        return bfs(beginWord,endWord,wordList);
     }
 };
