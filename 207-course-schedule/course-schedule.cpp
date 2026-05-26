63408bf5e39c71fa6c80a6e7282a2c9c6fa407b4
Shivam Kumar <theshivam23679@gmail.com>
2026-05-26 17:25:02 +0530

    Time: 6 ms (50.39%) | Memory: 19.4 MB (47.93%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,48 +1,42 @@
 class Solution {
 public:
-    vector<int> topoSort(int V, vector<int> adj[]) {
-        vector<int> inDegree(V, 0);
-
-        // Calculate in-degrees
-        for (int i = 0; i < V; i++) {
-            for (auto it : adj[i]) {
-                inDegree[it]++;
+    int topo(int n,vector<int>adj[]) {
+        
+        vector<int>indegree(n,0);
+        for(int i=0;i<n;i++){
+            for(auto it:adj[i]){
+                indegree[it]++;
             }
         }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0) q.push(i);
 
-        queue<int> q;
-        for (int i = 0; i < V; i++) {
-            if (inDegree[i] == 0) q.push(i);
         }
-
-        vector<int> ans;
-        while (!q.empty()) {
-            int node = q.front();
+        vector<int>toposort;
+        while(!q.empty()){
+            int node=q.front();
             q.pop();
-            ans.push_back(node);
+          toposort.push_back(node);
+          for(auto it:adj[node]){
+            indegree[it]--;
+            if(indegree[it]==0) q.push(it);
+          }
 
-            for (auto it : adj[node]) {
-                inDegree[it]--;
-                if (inDegree[it] == 0) q.push(it);
-            }
         }
-        return ans;
+        return toposort.size()==n;
     }
-
+  
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> adj[numCourses];
+       int n=prerequisites.size();
+        vector<int>adj[numCourses];
+       for(auto it:prerequisites){
+        int u=it[0];
+        int v=it[1];
+        adj[v].push_back(u);
+       }
+   return topo(numCourses,adj);
 
-        // Build graph
-        for (auto it : prerequisites) {
-            int u = it[0];
-            int v = it[1];
-            adj[v].push_back(u);  // v → u
-        }
 
-        // Run topo sort
-        vector<int> topo = topoSort(numCourses, adj);
-
-        // If topo sort doesn't include all courses → cycle exists
-        return topo.size() == numCourses;
     }
 };
