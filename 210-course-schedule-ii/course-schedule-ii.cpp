f189e606e685881de43f980052c5376dc1e7fc72
Shivam Kumar <theshivam23679@gmail.com>
2026-05-26 17:41:21 +0530

    Time: 10 ms (14.20%) | Memory: 18.4 MB (49.57%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,46 +1,41 @@
 class Solution {
 public:
-    vector<int> topoSort(int V, vector<int> adj[]) {
-	    vector<int> inDegree(V, 0);
-		for (int i = 0; i < V; i++) {
-			for(auto it : adj[i]) {
-			    inDegree[it]++;
-			}
-		}
-        vector<int> ans;
-	    queue<int> q;
-	    for(int i=0; i<V; i++) {
-	        if(inDegree[i] == 0) q.push(i);
-	    }
-	    while(!q.empty()) {
-	        int node = q.front();
-	        ans.push_back(node);
-	        q.pop();
-	        for(auto it : adj[node]) {
-	            inDegree[it]--;
-	            
-	            if(inDegree[it] == 0) q.push(it);
-	        }
-	    }
-	    
-	    return ans;
-    }
+   vector<int> topo(int V,vector<int>adj[]){
+      vector<int>indegree(V,0);
+      for(int i=0;i<V;i++){
+        for(auto it:adj[i]){
+            indegree[it]++;
+        }
+      }
+      queue<int>q;
+      for(int i=0;i<V;i++){
+        if(indegree[i]==0)q.push(i);
+      }
+      vector<int>toposort;
+      while(!q.empty()){
+        int node=q.front();
+          q.pop();
+          toposort.push_back(node);
+          for(auto it:adj[node]){
+             indegree[it]--;
+             if(indegree[it]==0) q.push(it);
+          }
+      }
+     if(toposort.size()==V) return toposort;
+
+   return {};
 
 
 
-    vector<int> findOrder(int numCourses, vector<vector<int>>
-    & prerequisites) {
-       vector<int>adj[numCourses];
-
-    for(auto it:prerequisites){
+   }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+     vector<int>adj[numCourses];
+      for(auto it:prerequisites){
         int u=it[0];
         int v=it[1];
         adj[v].push_back(u);
-    }
-    vector<int>topo=topoSort(numCourses,adj);
-      if(topo.size()< numCourses) return {};
-      return topo;
-    
-        
+
+      }
+       return topo(numCourses,adj); 
     }
 };
