c2ab3df00aea927005e0c72e4d49cb6b6a5c2fd1
Shivam Kumar <theshivam23679@gmail.com>
2026-05-30 15:19:23 +0530

    Time: 2 ms (32.64%) | Memory: 17.2 MB (43.46%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -12,46 +12,30 @@
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>>ans;
+        if(root==nullptr) return ans;
+      queue<TreeNode*>q;
+      q.push(root);
+     
+      while(!q.empty()){
+        int size=q.size();
+         vector<int>level;
+         for(int i=0;i<size;i++){
+        TreeNode*node=q.front();
+        q.pop();
+        level.push_back(node->val);
+         if(node->left!=nullptr){
+           q.push(node->left);
+         }
         
-     vector<vector<int>> ans; 
-        if (root == nullptr) {
-            // If the tree is empty, return an empty vector
-            return ans; 
+        if(node->right!=nullptr){
+            q.push(node->right);
         }
-        
-        // Create a queue to store nodes for level-order traversal
-        queue<TreeNode*> q; 
-        // Push the root node to the queue
-        q.push(root); 
-
-        while (!q.empty()) {
-            // Get the size of the current level
-            int size = q.size(); 
-            // Create a vector to store nodes at the current level
-            vector<int> level; 
-
-            for (int i = 0; i < size; i++) {
-               
-                TreeNode* node = q.front(); 
-               
-                q.pop(); 
-               
-                level.push_back(node->val); 
-
-                
-                if (node->left != nullptr) {
-                    q.push(node->left);
-                }
-                if (node->right != nullptr) {
-                    q.push(node->right);
-                }
-            }
-            
-            ans.push_back(level); 
-        }
-       
-        return ans; 
-
-
+         }
+      ans.push_back(level);
+      }
+      
+      
+   return ans;
     }
 };
