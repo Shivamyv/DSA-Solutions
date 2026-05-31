a228c07bb3048143644045d219749ef48ef4a269
Shivam Kumar <theshivam23679@gmail.com>
2026-05-31 14:32:52 +0530

    Time: 40 ms (17.08%) | Memory: 106.8 MB (16.20%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.cpp
+++ 2245-destroying-asteroids/destroying-asteroids.cpp
@@ -4,17 +4,17 @@ public:
       int n=asteroids.size();
       sort(asteroids.begin(),asteroids.end());
         long long sum=mass;
-       
-       
-
-        for (int x : asteroids) {
-            if (sum < x)
-                return false;
-
-            sum += x;
+        bool ans=false;
+        for(int i=0;i<n;i++){
+            if(sum >=asteroids[i]){
+                 ans=true;
+            sum+=asteroids[i];
+            }
+            else {
+                ans=false;
+            }
         }
-
-        return true;
+        return ans;
 
 
     }
