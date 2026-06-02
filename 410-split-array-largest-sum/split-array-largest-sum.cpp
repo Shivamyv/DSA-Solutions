74562068f8de8e99087e23094f58767272578483
Shivam Kumar <theshivam23679@gmail.com>
2026-06-02 14:57:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.71%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    int f(vector<int>& nums, int k){
-        int n=nums.size();
-
-      int partitions=1;
-      int subarraysum=0;
+  bool possible(vector<int>& nums, int k,int mid){
+      int n=nums.size();
+      int sum=0;
+      int cnt=1;
       for(int i=0;i<n;i++){
-        if(subarraysum+nums[i]<=k){
-           subarraysum+=nums[i];
-        } 
-         else {
-              
-                partitions++;
-                subarraysum = nums[i];
-            }
+        if(sum+nums[i]<=mid){
+            sum+=nums[i];
+        }
+        else{
+            sum=nums[i];
+            cnt++;
+
+        }
+
       }
-            return partitions;
-    }
+      return cnt<=k;
+  }
+
 
     int splitArray(vector<int>& nums, int k) {
       int n=nums.size();
       int low=*max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(), nums.end(), 0);
-        while(low<=high){
-            int mid=(low+high)/2;
-            int partitions=f(nums,mid);
-            if(partitions > k){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
+      int high=accumulate(nums.begin(),nums.end(),0);
+     int ans=0;
+       while(low<=high){
+        int mid=low+(high-low)/2;
+        if(possible(nums,k,mid)){
+            ans=mid;
+            high=mid-1;
         }
-      
-      
-      return low;
+        else{
+            low=mid+1;
+        }
+       }
+     return ans;
+
     }
 };
