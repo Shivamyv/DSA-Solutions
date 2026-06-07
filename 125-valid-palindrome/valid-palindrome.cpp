b07cc875e2b826ac798329a8fed9464ec45dc4ee
Shivam Kumar <theshivam23679@gmail.com>
2026-06-07 18:16:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (54.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,23 +1,20 @@
-class Solution{	
-	public:		
-		bool isPalindrome(string& s){
-		 int n=s.size();
-         int l=0,r=n-1;
-        if(s[l]==0 && s[r]==0) return false;
-         while(l<=r){
-             while (l < r && !isalnum(s[l])) l++;
-            // Move the right pointer to the previous alphanumeric character
-            while (l < r && !isalnum(s[r])) r--;
-            if (tolower(s[l]) != tolower(s[r])) {
-                return false;
-            }
-            
-           l++,r--;
-             
-         }
-            
-                return true;
-         
-		}
-        
+class Solution {
+public:
+    bool isPalindrome(string s) {
+    int n=s.size();
+    int i=0,j=n-1;
+    while(i<j){
+        while(i<j && !isalnum(s[i])) i++;
+        while(i<j && !isalnum(s[j])) j--;
+        if(tolower(s[i])!=tolower(s[j])) return false;
+
+        i++;
+        j--;
+    }
+    
+
+   return true;
+
+
+    }
 };
