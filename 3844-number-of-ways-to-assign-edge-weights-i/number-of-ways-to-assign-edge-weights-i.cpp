c581a0b04279a9d30ce3f08e482b9ae860d4e564
Shivam Kumar <theshivam23679@gmail.com>
2026-06-11 08:42:26 +0530

    Time: 476 ms (20.81%) | Memory: 352.9 MB (31.55%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+  int mod=1e9+7;
+
+    long long maxdepth(int node,vector<int>adj[],vector<int>&vis){
+         long long depth=0;
+          vis[node]=true;
+        for(auto it:adj[node]){
+            if(!vis[it]){
+          depth=max(depth,1+maxdepth(it,adj,vis)) ;
+         
+            }
+        }
+        return depth ;
+    }
+    long long power(long long a, long long b) {
+    long long ans = 1;
+
+    while (b > 0) {
+        if (b & 1)
+        ans = (ans * a) % mod;
+
+        a = (a * a) % mod;
+        b >>= 1;
+    }
+
+    return ans;
+}
+  
+    int assignEdgeWeights(vector<vector<int>>& edges) {
+        int n=edges.size()+1;
+       vector<int>adj[n+1];
+       for(auto it:edges){
+        int u=it[0];
+        int v=it[1];
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+       }
+       long long depthfind=0;
+       long long ways=0;
+    vector<int>vis(n+1,false);
+    
+       depthfind=maxdepth(1,adj,vis);
+
+        ways =power(2,depthfind-1);
+
+    
+   return ways;
+        
+    }
+};
