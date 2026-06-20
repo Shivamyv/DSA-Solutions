95bc93472dd3acf146079b801ca6dde297a53ea1
Shivam Kumar <theshivam23679@gmail.com>
2026-06-20 17:53:22 +0530

    Time: 73 ms (22.02%) | Memory: 193.5 MB (11.76%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,79 +1,65 @@
 class Solution {
 public:
-   vector<int>nextsee(vector<int> & arr){
-      int n=arr.size();
-      vector<int>ans(n);
-      stack<int>st;
-      for(int i=n-1;i>=0;i--){
-        int currelement=arr[i];
-      while(!st.empty() && arr[st.top()]>= arr[i]){
-        st.pop();
+   int mod=1e9+7;
 
-      }
-      ans[i] = !st.empty() ? st.top() : n;
-      st.push(i);
-   }
-   return ans;
-   }
-vector<int>ipsee(vector<int> & arr){
-    int n=arr.size();
-    vector<int>ans(n);
-    stack<int>st;
-    for(int i=0;i<n;i++){
-        int currelement=arr[i];
-        while(!st.empty() && arr[st.top()]> arr[i]){
-            st.pop();
+    vector<int> nse(vector<int>&arr){
+        int n=arr.size();
+        stack<int>st;
+        vector<int>ans(n);
+        for(int i=n-1;i>=0;i--){
+            int currelement=arr[i];
+            while(!st.empty() && arr[st.top()]>=currelement){
+                st.pop();
+            }
 
+            if(st.empty()){
+                ans[i]=n;
+            }
+            else{
+                ans[i]=st.top();
+            }
+            st.push(i);
         }
-         ans[i] = !st.empty() ? st.top() : -1;
-         st.push(i);
-    }
-    return ans;
+        return ans;
 
-}
+    }
+     vector<int> pse(vector<int>&arr){
+        int n=arr.size();
+        vector<int>ans(n);
+        stack<int>st;
+        for(int i=0;i<n;i++){
+            int currelement=arr[i];
+            while(!st.empty() && arr[st.top()]>currelement){
+                st.pop();
+            }
+            if(st.empty()){
+                ans[i]=-1;
+            }
+            else{
+                ans[i]=st.top();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
 
 
 
     int sumSubarrayMins(vector<int>& arr) {
-        int n=arr.size();
-      
-       vector<int> nse = 
-            nextsee(arr);
-        
-        vector<int> psee =
-           ipsee(arr);
-        
-       
-        
-        int mod = 1e9 + 7; 
-        
-        
-        int sum = 0;
-        
-        
-        for(int i=0; i < n; i++) {
-            
-            
-            int left = i - psee[i];
-            
-            
-            int right = nse[i] - i;
-            
-           
-            long long freq = left*right*1LL;
-            
-            
-            int val = (freq*arr[i]*1LL) % mod;
-            
-            
-            sum = (sum + val) % mod;
-        }
-        
-      
-        return sum;
-       
+         int n=arr.size();
+         int sum=0;
+         vector<int>nsee=nse(arr);
+         vector<int>psee=pse(arr);
+         for(int i=0;i<n;i++){
+            int right=nsee[i]-i;
+            int left=i-psee[i];
+            long long freq=right*left*1LL;
+            int val=(freq*arr[i]*1LL)%mod;
+            sum=(sum+val)%mod;
+         }
+   
+     return sum;
 
 
-         
     }
 };
