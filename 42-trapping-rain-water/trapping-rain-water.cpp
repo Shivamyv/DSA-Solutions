3e536d19c47ce71adbd0b1f604e9355715497059
Shivam Kumar <theshivam23679@gmail.com>
2026-06-20 09:04:33 +0530

    Time: 3 ms (16.93%) | Memory: 27 MB (37.06%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,37 +1,34 @@
-class Solution {
-public:
-   vector<int>prefixmax(vector<int> & arr,int  &n){
-    vector<int>prefix(n);
-    prefix[0]=arr[0];
-    for(int i=1;i<arr.size();i++){
-        prefix[i]=max(prefix[i-1],arr[i]);
+class Solution{
+   public: 
+
+
+int trap(vector<int>& height) {
+    stack<int> st;
+    int water = 0;
+
+    for(int i = 0; i < height.size(); i++) {
+
+        while(!st.empty() &&
+              height[i] > height[st.top()]) {
+
+            int bottom = st.top();
+            st.pop();
+
+            if(st.empty()) break;
+
+            int left = st.top();
+
+            int width = i - left - 1;
+
+            int h = min(height[left], height[i])
+                    - height[bottom];
+
+            water += width * h;
+        }
+
+        st.push(i);
     }
-   return prefix;
-  }
-  vector<int>suffixmax(vector<int> & arr,int n){
-    vector<int>suffix(n);
-    suffix[n-1]=arr[n-1];
-    for(int i=n-2;i>=0;i--){
-        suffix[i]=max(suffix[i+1],arr[i]);
-    }
-    return suffix;
-  }
 
-
-    int trap(vector<int>& height) {
-        int n=height.size();
-        int total=0;
-         vector<int>leftmax=prefixmax(height,n);
-         vector<int>rightmax=suffixmax(height,n);
-      for(int i=0;i<n;i++){
-      if(height[i]< leftmax[i] && height[i]<rightmax[i]){
-        total+=(min(rightmax[i],leftmax[i])-height[i]);
-      }
-
-
-      }
-       
-     return total;
-        
-    }
+    return water;
+}
 };
