40a97767d54b73282b22093154df2d55c4e597ae
Shivam Kumar <theshivam23679@gmail.com>
2026-06-21 17:25:34 +0530

    Time: 9 ms (10.02%) | Memory: 11.4 MB (63.53%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-     int n=num.size();
-     
-     string ans="";
-     stack<int>st;
-     for(int i=0;i<n;i++){
-        char currelement=num[i];
-        while(!st.empty() && k > 0 && st.top()>currelement ){
+        int n=num.size();
+       stack<char>st;
+       for(int i=0;i<n;i++){
+        char ch=num[i];
+        while(!st.empty() && st.top()>ch && k>0){
             st.pop();
             k--;
-        }
-        st.push(currelement);
-     }
-        while(!st.empty() && k>0 ){
-            st.pop();
-            k--;
-        }
-        if(st.empty()) return "0";
-        while(!st.empty()){
-            ans.push_back(st.top());
-            st.pop();
-        }
-        while(ans.size()>0 && ans.back()=='0'){
-            ans.pop_back();
-        }
-        reverse(ans.begin(),ans.end());
-        if(ans.empty()) return "0";
 
-     
+        }
+        st.push(ch);
+       }
+       while(!st.empty() && k>0) {
+        st.pop();
+        k--;
+       }
+       if(st.empty()) return "0";
+       string res="";
+       while(!st.empty()){
+        res.push_back(st.top());
+        st.pop();
+       }
+       while(res.size()>0 && res.back()=='0'){
+        res.pop_back();
+       }
+       reverse(res.begin(),res.end());
+       if(res.empty()) return "0";
+       return res;
 
-   return ans;
 
     }
 };
