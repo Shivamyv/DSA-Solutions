d6e68ae8c22d45e206e8d2d9116410131184b958
Shivam Kumar <theshivam23679@gmail.com>
2026-07-09 12:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (84.67%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -8,16 +8,22 @@ public:
         for(auto it:nums){
             mpp[it]++;
         }
-        vector<pair<int,int>>freq(mpp.begin(),mpp.end());
-        sort(freq.begin(),freq.end(),[](auto &a,auto &b){
-        return a.second>b.second;
-    });
-          for(int i=0;i<k;i++){
-            ans.push_back(freq[i].first);
+     priority_queue<pair<int,int>,
+     vector<pair<int,int>>,
+     greater<pair<int,int>>>pq;
+     for(auto it:mpp){
+        pq.push({it.second,it.first});
+        if(pq.size()>k){
+            pq.pop();
         }
 
-
-       return ans; 
-}
+     } 
+     while(!pq.empty()){
+        ans.push_back(pq.top().second);
+        pq.pop();
+     }
     
+     return ans;
+
+    } 
 };
