3191b8082905d4cc443851dd690b115512160033
Shivam Kumar <theshivam23679@gmail.com>
2026-07-14 17:36:17 +0530

    Time: 1894 ms (9.56%) | Memory: 58.4 MB (60.87%) - LeetSync

--- 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -2,35 +2,42 @@ class Solution {
 public:
   const long long mod=1e9+7;
   int dp[201][201][201];
-  
   int gcd(int a,int b){
      if(b==0)  return a;
     if(a==0) return b;
    return gcd(b,a%b);
       
   }
-   int f(int ind,int first,int second,vector<int>&nums){
-    int n=nums.size();
-    if(ind==n){
-        if(first==0 ||second==0) return 0;
-        else
-       return (first==second);
-    }
-    if(dp[ind][first][second]!=-1) return dp[ind][first][second];
-
-      int skip=f(ind+1,first,second,nums);
-      int seq1=f(ind+1,gcd(first,nums[ind]),second,nums);
-      int seq2=f(ind+1,first,gcd(second,nums[ind]),nums);
-      long long ans=(1LL*skip+seq1+seq2) %mod;
-      
-      return dp[ind][first][second]=ans;
-
-
-   }
+   
     int subsequencePairCount(vector<int>& nums) {
       int n=nums.size();
-      memset(dp,-1,sizeof(dp));
-      return f(0,0,0,nums);  
+      int mx=*max_element(nums.begin(),nums.end());
+      for(int first=0;first<201;first++){
+        for(int second=0;second<201;second++){
+            if(first==0 ||second==0){
+                dp[n][first][second]=0;
+            }
+        else{
+              dp[n][first][second]= (first==second);
+       }
+      }
+      }
+      for(int ind=n-1;ind>=0;ind--){
+        for(int first=0;first<=mx;first++){
+            for(int second=0;second<=mx;second++){
+       int skip=dp[ind+1][first][second];
+        int seq1=dp[ind+1][gcd(first,nums[ind])][second];
+        int seq2=dp[ind+1][first][gcd(second,nums[ind])];
+        long long ans=(1LL*skip+seq1+seq2) %mod;
+          dp[ind][first][second]=ans;
+
+            }
+        }
+      }
+
+    return dp[0][0][0];
+
+      
 
     }
 };
