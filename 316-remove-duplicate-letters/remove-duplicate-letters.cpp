74ccbded8dd7e4538c085cdd95e45f85d6bc0d0d
Shivam Kumar <theshivam23679@gmail.com>
2026-07-19 11:48:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (84.28%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -9,14 +9,14 @@ public:
         }
         string st="";
         for(int i=0;i<n;i++){
-            char ch=s[i];
-            if(visited[ch-'a']) continue;
-            while(!st.empty() && st.back() > ch && lastindex[st.back()-'a']>i){
+        
+            if(visited[s[i]-'a']) continue;
+            while(!st.empty() && st.back() > s[i] && lastindex[st.back()-'a']>i){
                 visited[st.back()-'a']=false;
                 st.pop_back();
             }
-            st.push_back(ch);
-            visited[ch-'a']=true;
+            st.push_back(s[i]);
+            visited[s[i]-'a']=true;
         }
         return st;
     }
