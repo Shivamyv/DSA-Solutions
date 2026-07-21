7d215de68ceb49fd76e3ee14504fe8d6ca15d15e
Shivam Kumar <theshivam23679@gmail.com>
2026-07-21 12:08:16 +0530

    Time: 35 ms (47.09%) | Memory: 31.4 MB (59.39%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-   int f(string s,string t ){
+int f(string &s,string &t){
     int n=s.size();
-    int m=t.size();
-    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-
-    for(int j=0;j<=m;j++) dp[0][j]=0;
-    for(int i=0;i<=n;i++) dp[i][0]=0;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(s[i-1]==t[j-1]) dp[i][j] =1+dp[i-1][j-1];
-            else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-
+    vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+    
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(s[i-1]==t[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
         }
-    }
-    return dp[n][m];
-
-   }
-
-    int longestPalindromeSubseq(string s) {
-       string t=s;
-       reverse(t.begin(),t.end());
-       return f(s,t);  
-
-    }
+     }
+     return dp[n][n];
+}
 
     int minInsertions(string s) {
-       return s.size()- longestPalindromeSubseq(s);
+    int n=s.size();
+    string t=s;
+    reverse(t.begin(),t.end());
+    int  longest=f(s,t);
+
+    return n-longest;
+
+
+
     }
-    
 };
