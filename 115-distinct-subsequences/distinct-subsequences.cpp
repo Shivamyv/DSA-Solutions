f3913d2e02dba52611179558bf40594b3fb6eac6
Shivam Kumar <theshivam23679@gmail.com>
2026-07-21 19:17:03 +0530

    Time: 38 ms (24.12%) | Memory: 27.1 MB (46.99%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    int f(int i,int j,string s, string t,vector<vector<int>>&dp){
-     if(j==0) return 1;
-     if(i==0) return 0;
-     
-     if(dp[i][j]!=-1) return dp[i][j];
-     if(s[i-1]==t[j-1]){
-    return dp[i][j] =f(i-1,j-1,s,t,dp)+f(i-1,j,s,t,dp);
-    }
-     return dp[i][j]=f(i-1,j,s,t,dp);
+const long long mod=1e9+7;
+   int f(int i,int j,string &s,string &t, vector<vector<int>>&dp){
+        int n=s.size();
+        int m=t.size();
+       if(j<0) return 1;
+       if(i<0) return 0; 
+      if(dp[i][j]!=-1) return dp[i][j];
 
+        int result=0;
+        if(s[i]==t[j]){
+            int move=f(i-1,j-1,s,t,dp);
+            int notmove=f(i-1,j,s,t,dp);
+            result=(move+notmove)%mod;
+            
+        }
+        else{
+            result=f(i-1,j,s,t,dp);
+        }
+       return dp[i][j]= result;
 
-    }
-
+   }
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
-        for(int i=0;i<=n;i++) dp[i][0]=1;
-        
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-         if(s[i-1]==t[j-1]){
-                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
-    }
-         else{
-             dp[i][j]=dp[i-1][j];
-         }
-            }
-        }
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        return f(n-1,m-1,s,t,dp);
 
 
-        return(int)dp[n][m];
+
     }
 };
