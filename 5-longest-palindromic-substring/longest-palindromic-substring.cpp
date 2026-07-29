ff9f5cdb8307d875cf64676ba3d07f7e85682ee5
Shivam Kumar <theshivam23679@gmail.com>
2026-07-30 01:16:59 +0530

    Time: 354 ms (13.50%) | Memory: 292 MB (6.83%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
 bool f(int i,int j,string &s, vector<vector<int>>&dp){
-   if(i>=j) return 1;
+   if(i>=j) return true;
    if(dp[i][j]!=-1) return dp[i][j];
    if(s[i]==s[j]){
-     return dp[i][j]=  f(i+1,j-1,s,dp);
+     return dp[i][j]= f(i+1,j-1,s,dp);
    }
    return dp[i][j]=false;
 
