8de7a31105baf8c2ac40a94c045130a68b626372
Shivam Kumar <theshivam23679@gmail.com>
2026-08-04 01:22:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (38.20%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stack;
+        int n=s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+           if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
+            st.push(s[i]);
+           }
+           else if(st.empty() || abs(s[i]-st.top())>2) return false;
+           else{
+            st.pop();
+           }
 
-        for (int i = 0; i < s.length(); i++)
-        {
-            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
-            {
-                stack.push(s[i]);
-            }   
-            else if( stack.empty() || abs(s[i] - stack.top()) > 2 )
-            {
-                return false;
-            }
-            else
-            {
-                stack.pop();
-            }
         }
-        return stack.empty();
+        return st.empty();
+
     }
 };
