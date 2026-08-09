fe2fc1f32322de7cf19923f5cfc992b5154f0897
Shivam Kumar <theshivam23679@gmail.com>
2026-08-09 22:36:09 +0530

    Time: 132 ms (52.98%) | Memory: 110.4 MB (40.96%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
+bool f(int i,int target,vector<int>&nums,vector<vector<int>>& dp){
+    if(target==0) return true;
+
+    if(i==0) return nums[0]==target;
+
+    if(dp[i][target]!=-1) return dp[i][target];
+
+    bool nottake=f(i-1,target,nums,dp);
+
+    bool take=false;
+    if(nums[i]<=target){
+        take=f(i-1,target-nums[i],nums,dp);
+    }
+    return dp[i][target]= nottake || take;
+}
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
+        int n=nums.size();
+       int sum=0;
+    
+       for(int i=0;i<n;i++){
+        sum+=nums[i];
 
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-
-        if (sum % 2 != 0)
-            return false;
-
-        int target = sum / 2;
-        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
-
-        // Initialization
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = true;
+       }
+       if(sum%2==1) return false;
+       else {
+        int k=sum/2;
+        vector<vector<int>> dp(n, vector<int>(k + 1, -1));
+        for(int i=0;i<n;i++){
+            dp[n-1];
         }
+        return f(n-1,k,nums,dp);
+       }
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
 
-        return dp[n][target];
     }
 };
