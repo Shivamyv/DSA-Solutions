928cb63ac5c6117e3636c33f1e40e8984b8f30a9
Shivam Kumar <theshivam23679@gmail.com>
2026-08-13 17:39:30 +0530

    Time: 119 ms (71.38%) | Memory: 19 MB (56.58%) - LeetSync

---
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+int f(int i,string &s,vector<int>&dp){
+    int n=s.size();
+    if(i>=n) return 0;
+    if(dp[i]!=-1) return dp[i];
+
+      int ans=n-i;
+      vector<int>freq(26,0);
+      
+      for(int k=i;k<n;k++){
+        freq[s[k]-'a']++;
+
+         int frequency=0;
+        bool valid=true;
+
+        for(int x=0;x<26;x++){
+            if(freq[x]>0){
+                if(frequency==0){
+                    frequency=freq[x];
+
+                }
+                else if(freq[x]!=frequency){
+                    valid=false;
+                    break;
+
+                }
+            }
+        }
+
+     if(valid){
+        ans=min(ans,1+f(k+1,s,dp));
+
+     }
+
+     }
+    return  dp[i]=ans;
+
+
+
+}
+    int minimumSubstringsInPartition(string s) {
+       int n=s.size();
+       vector<int>dp(n,-1);
+        return f(0,s,dp);
+
+    }
+};
