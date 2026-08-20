82275ca0659a91faf8ff645e09f366d9614bb75e
Shivam Kumar <theshivam23679@gmail.com>
2026-08-21 00:51:13 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (66.37%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
-        vector<int> arr1(1, nums[0]), arr2(1, nums[1]);
-        for(int i = 2;i < nums.size();i++){
-            if(arr1.back() > arr2.back()) arr1.push_back(nums[i]);
-            else arr2.push_back(nums[i]);
-        }
+        int n=nums.size();
+        vector<int>arr1(1,nums[0]);
+        vector<int>arr2(1,nums[1]);
+        for(int i=2;i<n;i++){
+            if(arr1.back()>arr2.back()) arr1.push_back(nums[i]);
+            else{
+                arr2.push_back(nums[i]);
+            }
 
-        arr1.insert(arr1.end(), arr2.begin(), arr2.end());
+        }
+       arr1.insert(arr1.end(),arr2.begin(),arr2.end());
+       
         return arr1;
+        
     }
 };
-
-auto init = []()
-{ 
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    return 'c';
-}();
