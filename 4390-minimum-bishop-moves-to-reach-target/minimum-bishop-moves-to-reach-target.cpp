e0a2fc7a15ec9ae4242add83ac69c6ea2aad8499
Shivam Kumar <theshivam23679@gmail.com>
2026-08-30 19:39:14 +0530

    Time: 0 ms (100.00%) | Memory: 50.5 MB (37.50%) - LeetSync

---
+++ 4390-minimum-bishop-moves-to-reach-target/minimum-bishop-moves-to-reach-target.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    // int dr[]={-1,-1,+1,+1};
+    // int dc[]={-1,+1,+1,-1};
+
+    int minBishopMoves(vector<int>& source, vector<int>& target) {
+         
+        if((source[0]+source[1]) % 2 != (target[0]+target[1]) % 2) return -1;
+        
+        
+         if(source[0]+source[1]==target[0]+target[1] || 
+         source[0]-source[1]==target[0]-target[1]) return 1;
+
+
+    return 2;
+
+    }
+};
