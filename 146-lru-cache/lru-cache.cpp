215c08a299adbf9bc0ef86386593b0c335b0fe51
Shivam Kumar <theshivam23679@gmail.com>
2026-09-15 00:04:04 +0530

    Time: 23 ms (69.59%) | Memory: 50.5 MB (63.68%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,51 +1,42 @@
 class LRUCache {
 public:
-    
-    vector<pair<int, int>> cache;
-    int n;
+    list<int> dll; //it contains the key
+    map<int, pair<list<int>::iterator, int>> cache; //key->(list_node, value)
+    int capacity;
     
     LRUCache(int capacity) {
-        n = capacity;
+        this->capacity = capacity;
+    }
+    
+    void makeMostRecentlyUsed(int key) {
+        dll.erase(cache[key].first);
+        dll.push_front(key);
+        cache[key].first = dll.begin();
     }
     
     int get(int key) {
+        if(!cache.count(key))
+            return -1;
         
-        for(int i = 0; i<cache.size(); i++) {
-            
-            if(cache[i].first == key) {
-                int val = cache[i].second;
-                
-                pair<int, int> temp = cache[i];
-                cache.erase(cache.begin()+i);
-                cache.push_back(temp);
-                
-                return val;
-            }
-            
-        }
-        
-        return -1;
-        
+        makeMostRecentlyUsed(key);
+        return cache[key].second;
     }
     
     void put(int key, int value) {
-        
-        for(int i = 0; i<cache.size(); i++) {
-            
-            if(cache[i].first == key) {
-                cache.erase(cache.begin()+i);
-                cache.push_back({key, value});
-                return;
-            }
-        }
-        
-        if(cache.size() == n) {
-            cache.erase(cache.begin());
-            cache.push_back({key, value});
+        if(cache.count(key)) {
+            cache[key].second = value;
+            makeMostRecentlyUsed(key);
         } else {
-            cache.push_back({key, value});
+            dll.push_front(key);
+            cache[key] = {dll.begin(), value};
+            capacity--;
         }
         
+        if(capacity < 0) {
+            cache.erase(dll.back());
+            dll.pop_back();
+            capacity++;
+        }
     }
 };
 
