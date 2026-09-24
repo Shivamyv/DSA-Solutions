6af0699cf11a61d68e4bb445ec8fd4f0f1be1cb6
Shivam Kumar <theshivam23679@gmail.com>
2026-09-25 01:10:12 +0530

    Time: 3 ms (58.35%) | Memory: 19.4 MB (29.09%) - LeetSync

--- 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
+++ 3869-smallest-index-with-digit-sum-equal-to-index/smallest-index-with-digit-sum-equal-to-index.py
@@ -3,11 +3,11 @@ class Solution:
         n=len(nums)
         for i in range(0,n):
             sum=0
-
-            while nums[i]>0 :
-             cnt=nums[i]%10
+            x=nums[i]
+            while x>0 :
+             cnt=x%10
              sum+=cnt
-             nums[i]//=10
+             x//=10
             if(sum==i):
              return i
         return -1
